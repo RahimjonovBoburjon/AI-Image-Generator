eb2f58bd9a22cca505a2090e0d04c4cd0efd0502
Boburjon <shadow88bob@gmail.com>
2025-03-12 16:42:50 +0500

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
     <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center p-4">
         <Header />
         <router-view />
+        <Footer />
     </div>
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
