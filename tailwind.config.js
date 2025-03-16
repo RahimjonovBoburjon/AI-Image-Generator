40c5d911d0b395869fb284f569bc82050b9c6463
Boburjon <shadow88bob@gmail.com>
2025-03-16 22:38:58 +0500

    Update Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      boxShadow: {
-        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.5)',
-      },
-      backdropBlur: {
-        xs: '2px',
-      }
-    }
+    extend: {},
   },
   plugins: [],
 };
